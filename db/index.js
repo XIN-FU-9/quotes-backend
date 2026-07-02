146cf30d4a4c684935d5aac495bb9ddc06eeab20
XIN FU <xin.fu.ttp@gmail.com>
2026-07-02 14:36:52 -0400

    add Database Connection

--- db/index.js
+++ db/index.js
@@ -4,13 +4,17 @@
 // Before you start:
 //   Run this in quotes-backend: npm install sequelize pg pg-hstore
 //   Then create a "quotes" database in Postico or pgAdmin.
-//
+
+
 // YOUR TASKS:
 //   1. Import Sequelize from the 'sequelize' package
+import {Sequelize} from ("sequelize"); 
+// const {Sequelize} = ("sequelize");
 //   2. Create a new Sequelize instance connected to your quotes database
 //      Use this connection string: postgres://localhost:5432/quotes
+const dbConnection = new Sequelize("postgrespostgres://localhost:5432/quotes:")
 //   3. Export the sequelize database instance
-//
+Module.exports(dbConnection);
 // Every other file that needs the database imports from here.
 // Never create a second Sequelize connection in another file.
 // ============================================================
